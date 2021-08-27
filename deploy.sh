13f62daacac8cdbc609c18e2ad61a457145c765d
yosion-p <86145334+yosion-p@users.noreply.github.com>
2021-08-27 17:28:17 +0800

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -21,6 +21,17 @@ git commit -m 'deploy'
 
 # 如果发布到 https://<USERNAME>.github.io/<REPO>
 # git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
+# 配置ssh
+if [ -n "${ACCESS_TOKEN_DEPLOY}" ]; then
+    echo "设置 ACCESS_TOKEN_DEPLOY"
+    SSH_DIR="${HOME}/.ssh"
+    mkdir "${SSH_DIR}"
+    ssh-keyscan -t rsa github.com >"${SSH_DIR}/known_hosts"
+    echo "${ACCESS_TOKEN_DEPLOY}" >"${SSH_DIR}/id_rsa"
+    chmod 400 "${SSH_DIR}/id_rsa"
+    remote_repo="git@github.com:${PUBLISH_REPOSITORY}.git"
+fi
+# https://github.com/originjs/docs.git
 git push -f git@github.com:originjs/docs.git mian:gh-pages
 
 cd -
